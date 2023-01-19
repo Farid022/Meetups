3feb865d41b72399eff2256a841e7eeef9f0dc3a
Muhammad Farid Ullah <faridkhan125janan@gmail.com>
2023-01-19 11:55:57 +0500

    Initial Commit

---
+++ Meetups/MeetupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetupsApp.swift
+//  Meetups
+//
+//  Created by Muhammad Farid Ullah on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MeetupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
